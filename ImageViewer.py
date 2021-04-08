1e043770ee1b19bc916b826b88481a657b1dea11
Quentyn May <78805341+Qmay654@users.noreply.github.com>
2021-04-08 08:47:03 -0400

    Update ImageViewer.py
    Added more comments to make each thing more clear as to what it is doing

--- ImageViewer.py
+++ ImageViewer.py
@@ -19,38 +19,50 @@ label = Label(image=myimg1)
 label.grid(row=0, column=0, columnspan=3)
 
 
+# forward button function
 def forward(image_number):
     global label
     global foward_button
     global back_button
 
+    # getting rid of the image
     label.grid_forget()
+
+    # redefining the image and the buttons
     label = Label(image=imagelist[image_number - 1])
     foward_button = Button(root, text='>>', command=lambda: forward(image_number + 1))
     back_button = Button(root, text='<<', command=lambda: back(image_number - 1))
 
+    # if the button is the last image, disable the button
     if image_number == len(imagelist):
         foward_button = Button(root, text='>>', state=DISABLED)
 
+    # place the buttoms back on the screen
     label.grid(row=0, column=0, columnspan=3)
     back_button.grid(row=1, column=0)
     foward_button.grid(row=1, column=2)
     return
 
 
+# back button function 
 def back(image_number):
     global label
     global foward_button
     global back_button
 
+    # getting rid of the image
     label.grid_forget()
+
+    # redefining the image and the buttons
     label = Label(image=imagelist[image_number - 1])
     foward_button = Button(root, text='>>', command=lambda: forward(image_number + 1))
     back_button = Button(root, text='<<', command=lambda: back(image_number - 1))
 
+    # if the button is the last image, disable the button
     if image_number == 1:
         back_button = Button(root, text='<<', state=DISABLED)
 
+    # place the buttoms back on the screen
     label.grid(row=0, column=0, columnspan=3)
     back_button.grid(row=1, column=0)
     foward_button.grid(row=1, column=2)
@@ -63,8 +75,10 @@ back_button = Button(root, text='<<', state=DISABLED)
 button_quit = Button(root, text="Exit", command=root.quit)
 foward_button = Button(root, text='>>', command=lambda: forward(2))
 
+# placing the buttons for the first time
 back_button.grid(row=1, column=0)
 button_quit.grid(row=1, column=1)
 foward_button.grid(row=1, column=2)
 
 root.mainloop()
+
