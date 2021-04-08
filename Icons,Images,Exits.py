41aeb6a3ff66989e539f349d86d97db20e498f22
Quentyn May <78805341+Qmay654@users.noreply.github.com>
2021-04-08 08:52:18 -0400

    Update Icons,Images,Exits.py
    Just made it look a little nicer

--- Icons,Images,Exits.py
+++ Icons,Images,Exits.py
@@ -1,11 +1,11 @@
 from tkinter import *
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Icons, Images, and Exits")
 root.iconbitmap('C:/ImageViewer/hnet.com-image.ico')
 
-myimg= ImageTk.PhotoImage(Image.open('C:/ImageViewer/dice.png'))
+myimg = ImageTk.PhotoImage(Image.open('C:/ImageViewer/dice.png'))
 label = Label(image=myimg)
 label.pack()
 
